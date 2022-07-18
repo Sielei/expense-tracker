44f4fb8b2bc98d568139b81f46d891b7bd163aa9
hsielei@seaburysolutions.com <hsielei@seaburysolutions.com>
2022-07-18 15:17:04 +0300

    created rest controller for user module

--- expense-tracker-backend/backend-application/src/main/java/com/et/backend/application/rest/UserController.java
+++ expense-tracker-backend/backend-application/src/main/java/com/et/backend/application/rest/UserController.java
@@ -1,13 +1,40 @@
 package com.et.backend.application.rest;
 
+import com.et.backend.application.dto.PasswordDto;
+import com.et.backend.application.util.JwtUtil;
+import com.et.user.application.service.dto.UserDto;
+import com.et.user.application.service.ports.input.service.UserApplicationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserApplicationService userApplicationService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+
+
+    @PutMapping("/update-user-details")
+    public ResponseEntity<UserDto> updateUserDetails(@RequestAttribute("userId") UUID userId, @RequestBody UserDto userDto){
+
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<String> updatePassword(@RequestBody PasswordDto passwordDto){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
 
 }
